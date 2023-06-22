495c455b4de925141dbba300c2cabd34b54e1c90
Pablo Márquez <pablomarquezartesano@gmail.com>
2023-06-22 15:49:04 +0200

    Planteamiento de la estructura

--- CvPabloMárquez/HomeViewController.swift
+++ CvPabloMárquez/HomeViewController.swift
@@ -13,7 +13,7 @@ class HomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        view.backgroundColor = UIColor.systemBlue
         // Do any additional setup after loading the view.
     }
 
