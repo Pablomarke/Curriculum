4ecabc7fd2cafd594d71dd5173fe641f02e8575e
Pablo Márquez <pablomarquezartesano@gmail.com>
2023-06-22 14:28:22 +0200

    Planteamiento de estructura básico

---
+++ CvPabloMárquez/BioViewController.swift
@@ -0,0 +1,17 @@
+//
+//  BioViewController.swift
+//  CvPabloMárquez
+//
+//  Created by Pablo Márquez Marín on 22/6/23.
+//
+
+import UIKit
+
+class BioViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ CvPabloMárquez/TrainingViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TrainingViewController.swift
+//  CvPabloMárquez
+//
+//  Created by Pablo Márquez Marín on 22/6/23.
+//
+
+import UIKit
+
+class TrainingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = UIColor.blue
+    }
+
+
+}
