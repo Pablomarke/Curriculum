32e998b74513d47d0ac56b13e227ada5a85d33b6
Pablo Márquez <pablomarquezartesano@gmail.com>
2023-06-25 17:37:26 +0200

    aplicacion de fuentes personalizadas

--- CvPabloMárquez/BioViewController.swift
+++ CvPabloMárquez/BioViewController.swift
@@ -7,11 +7,17 @@
 
 import UIKit
 
+
 class BioViewController: UIViewController {
+    
+    @IBOutlet weak var bioLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.systemFill
+        titleLabel.textColor = UIColor.systemMint
+        bioLabel.textColor = UIColor.systemMint
         
     }
 }
