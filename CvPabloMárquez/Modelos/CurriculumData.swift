d2fd43cae6bf88edd7876d8bedcaf68a1f9858b6
Pablo Márquez <pablomarquezartesano@gmail.com>
2023-06-28 12:11:38 +0200

    Datos del curriculum

---
+++ CvPabloMárquez/Modelos/CurriculumData.swift
@@ -0,0 +1,15 @@
+//
+//  DatosCurriculum.swift
+//  CvPabloMárquez
+//
+//  Created by Pablo Márquez Marín on 27/6/23.
+//
+
+import Foundation
+
+let bio:String = "Desarrollador de aplicaciones móviles IOs. \nCon ganas de desarrollar mi experiencia en KeepCoding y pode aplicar todo lo aprendido. Con gran sentido visual gracias a mi trabajo como fotógrafo y mi capacidad para trabajar en equipo, gracias a mi experiencia como director de teatro. Siempre ligado a las nuevas tecnologías, tanto en el uso de programas de edición como a la gestión de datos."
+
+let estudes: String =
+    "*Bootcamp Desarrollo de APP móviles (Full stack) \nkeepCoding 2023 Bootcamp \n\n*Aprende a Programar desde cero (Full stack junior) \nkeepCoding 2023 \n\n*Ios y Swift 5.7 Curso completo desde cero a ProfesionalCurso \nOnline Udemy 2022"
+
+let experience: String = "*2020-2022 Avante E-commerce S.l. \n Trabajo de fotógrafo de e-commerce a tiempo completo. Desde la captura de imagen hasta su procesamiento con herramientas de edición. \n\n*2014-2022 varias empresas y cuenta propia\nTrabajos diversos por cuenta propia y ajena. Tanto fotografía de estudio como cubriendo eventos empresariales y sociales. "
